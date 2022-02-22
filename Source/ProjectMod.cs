71160d6223e5a49604e997913332693d4fb5a300
c-ssen <ex010+1@live.com>
2022-02-22 14:51:59 -0500

    Added startup constructors

--- Source/ProjectMod.cs
+++ Source/ProjectMod.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using Verse;
 
 namespace ProjectMod
 {
-    public class ProjectMod
+    [StaticConstructorOnStartup]
+    public static class ProjectMod
     {
+        static ProjectMod()
+		{
+            Log.Message("Static Project Mod Class Loaded");
+		}
     }
+
+	public class ProjectModConfig : Mod
+	{
+		public ProjectModConfig(ModContentPack content) : base(content)
+		{
+			Log.Message("Inherited Project Mod Class Loaded");
+		}
+	}
 }
